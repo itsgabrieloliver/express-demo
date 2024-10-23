8d3916498593fe9780e642e86ec48a223837bf63
Gabriel Oliver <straxxmusicteam@gmail.com>
2024-10-23 14:04:04 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => {
+	res.send('hello world')
+})
+
+app.get('/test', (req, res) => {
+	res.send('test')
+})
+
+app.listen(port, () => {
+	console.log('running')
+})
